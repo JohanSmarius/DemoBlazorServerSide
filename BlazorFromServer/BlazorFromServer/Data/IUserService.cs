241c27835cceb3b3b45a3d036afe3047e021f0ec
Johan Smarius <jsmarius@hotmail.com>
2019-04-30 14:05:12 +0200

    Updated the project to Preview 4. The old structure is moved to the new single project approach.

---
+++ BlazorFromServer/BlazorFromServer/Data/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Core.Domain;
+
+namespace BlazorFromServer.Data
+{
+    public interface IUserService
+    {
+        void LoadPersons();
+        List<User> AvailablePersons { get; }
+        void DeleteLastPerson();
+    }
+}
---
+++ BlazorFromServer/BlazorFromServer/JavaScriptInterop/JavaScriptInterop.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace BlazorFromServer.JavaScriptInterop
+{
+    public class JavaScriptInterop : IJavaScriptInterop
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public JavaScriptInterop(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime ?? throw new System.ArgumentNullException(nameof(jsRuntime));
+        }
+
+        public Task<bool> Confirm(string message)
+        {
+            return _jsRuntime.InvokeAsync<bool>("helperfunction.confirm", message);
+        }
+    }
+}
